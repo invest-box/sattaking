73ce334f9a924657d2dfd116ecb46644127e3150
invest-box <investbox0303@gmail.com>
2022-08-15 16:22:43 +0530

    3rd commit

--- satta_app/src/links.js
+++ satta_app/src/links.js
@@ -1,19 +1,19 @@
-// var localLinks = {
-//   loginLink: '/login',
-//   baseUrlLink: '/',
-//   userloginLink: '/userlogin',
-//   userdataLink: '/userdata',
-//   mainDataLink: '/getmaindata',
-//   allDataLink:'/alldata'
-// };
-
 var localLinks = {
-  loginLink: 'http://localhost:8080/login',
-  baseUrlLink: 'http://localhost:8080/',
-  userloginLink: 'http://localhost:8080/userlogin',
-  userdataLink: 'http://localhost:8080/userdata',
-  mainDataLink: 'http://localhost:8080/getmaindata',
-  allDataLink: 'http://localhost:8080/alldata',
+  loginLink: '/login',
+  baseUrlLink: '/',
+  userloginLink: '/userlogin',
+  userdataLink: '/userdata',
+  mainDataLink: '/getmaindata',
+  allDataLink:'/alldata'
 };
+
+// var localLinks = {
+//   loginLink: 'http://localhost:8080/login',
+//   baseUrlLink: 'http://localhost:8080/',
+//   userloginLink: 'http://localhost:8080/userlogin',
+//   userdataLink: 'http://localhost:8080/userdata',
+//   mainDataLink: 'http://localhost:8080/getmaindata',
+//   allDataLink: 'http://localhost:8080/alldata',
+// };
 
 export default localLinks;
